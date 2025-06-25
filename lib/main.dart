6d954f9478358d3e0bc2c8cb8e764817a3afe0a6
Ashraf-Khaled-2005 <ashrafkhaled200514@gmail.com>
2025-06-25 17:34:29 +0300

    setup firebase lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/const/App_color.dart';
 import 'package:movie_app/core/utils/Router.dart';
-import 'package:movie_app/features/splash/presentation/views/splash_view.dart';
+import 'package:movie_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
